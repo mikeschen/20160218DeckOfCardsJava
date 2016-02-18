e3e9d0cdfbda9c1fa9899fdede6f7fb393438bb8
Kyle Derrick and Michael Schennum <mikeschenphoto@gmail.com>
2016-02-18 10:47:57 -0800

    adds initial files

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,31 @@
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+  public WebDriver getDefaultDriver() {
+      return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test
+  public void rootTest() {
+      goTo("http://localhost:4567/");
+      assertThat(pageSource()).contains("YOUR H1 HEADER");
+  }
+
+  @Test
+  public void returnsTotalScore() {
+    goTo("http://localhost:4567");
+    fill("#word").with("dog");
+    submit(".btn");
+    assertThat(pageSource()).contains("d-g");
+  }
+}
